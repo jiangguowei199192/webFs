347e90dfdc2716c0d5e41fa43b69d7a57fb3f564
zhaoaoxun <zhaoaoxun@gdu-tech.com>
2020-09-27 16:58:32 +0800

    开发中

---
+++ src/views/decisionSystem/components/RealtimeDataMenu.vue
@@ -0,0 +1,32 @@
+<template>
+  <div class="containerStyle">
+    <PoliceRankingMenu></PoliceRankingMenu>
+  </div>
+</template>
+
+<script>
+import PoliceRankingMenu from './PoliceRankingMenu'
+
+export default {
+  components: {
+    PoliceRankingMenu
+  },
+  data () {
+    return {
+
+    }
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+.containerStyle {
+  position: absolute;
+  right: 30px;
+  top: 60px;
+  width: 380px;
+  // height: 850px;
+  background: lemonchiffon;
+}
+
+</style>
