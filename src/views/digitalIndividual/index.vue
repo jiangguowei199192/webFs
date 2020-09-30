7bb56aeb548eb6eb5292eca39ae0e1f6ec440561
zhaoaoxun <zhaoaoxun@gdu-tech.com>
2020-09-30 17:09:02 +0800

    UI全部完成

--- src/views/digitalIndividual/index.vue
+++ src/views/digitalIndividual/index.vue
@@ -54,10 +54,11 @@
           >
         </div>
 
-        <div class="deleteBtn">批量删除</div>
+        <div class="deleteBtn" @click="deleteClick">批量删除</div>
         <!-- <div class="clearBtn">清空</div> -->
         <div class="chooseCount">
-          已选<span style="color: #ff0000">10</span>项
+          已选<span style="color: #ff0000">{{ selectedItem.length }}</span
+          >项
         </div>
       </div>
 
@@ -151,14 +152,12 @@
                 size="mini"
                 type="danger"
                 @click="resolve(scope.$index, scope.row)"
-                style="
-                  width: 74px;
-                  height: 26px;
-                  background: #1eb0fc;
-                  font-size: 16px;
-                  border: none;
-                "
+                class="unHandleBtn"
+                v-if="scope.row.status === '未处置'"
                 >未处置</el-button
+              >
+              <el-button size="mini" type="danger" v-else class="handleBtn"
+                >已处置</el-button
               >
             </template>
           </el-table-column>
@@ -182,36 +181,73 @@
         <div class="detailTitle">案件信息</div>
         <div class="detailTitleLine"></div>
         <div>
-          <div class="detailText1 detailText2">
-            案件编号：888888888888888888
-          </div>
-          <div class="detailText1">信息来源：888888888888888888</div>
+          <div class="detailText1 detailText2">案件编号：{{detailDlg.bianHao}}</div>
+          <div class="detailText1">信息来源：{{detailDlg.source}}</div>
         </div>
         <div>
-          <div class="detailText1 detailText2">举报人：举报人</div>
-          <div class="detailText1">举报电话：888888888888888888</div>
+          <div class="detailText1 detailText2">举报人：{{detailDlg.people}}</div>
+          <div class="detailText1">举报电话：{{detailDlg.phone}}</div>
         </div>
-        <div class="detailText3">举报地址：888888888888888888</div>
+        <div class="detailText3">举报地址：{{detailDlg.address}}</div>
         <div>
           <div class="detailText1 detailText2">
-            举报时间：888888888888888888
+            举报时间：{{detailDlg.time}}
           </div>
-          <div class="detailText1">案件所属：888888888888888888</div>
+          <div class="detailText1">案件所属：{{detailDlg.belong}}</div>
         </div>
-        <div class="detailText3">简要描述：888888888888888888</div>
-        <div class="detailText3">重点记录：888888888888888888</div>
+        <div class="detailText3">简要描述：{{detailDlg.description}}</div>
+        <div class="detailText3">重点记录：{{detailDlg.mainRecord}}</div>
 
         <div class="detailTitle" style="margin-top: 33px">处置信息</div>
         <div class="detailTitleLine"></div>
-        <div class="detailText3">处置结果：888888888888888888</div>
+        <div class="detailText3">处置结果：{{detailDlg.handleResult}}</div>
         <div>
           <div class="detailText1 detailText2">
-            处置时间：888888888888888888
+            处置时间：{{detailDlg.handleTime}}
           </div>
-          <div class="detailText1">处置人：888888888888888888</div>
+          <div class="detailText1">处置人：{{detailDlg.handlePeople}}</div>
         </div>
 
         <div class="npdCancel" @click="detailCancelClick">关闭</div>
+      </div>
+    </el-dialog>
+
+    <el-dialog :visible.sync="showHandle" width="960px" class="newPoliceDlg">
+      <div>
+        <div class="npdTitleSty">处置记录</div>
+        <el-form
+          ref=""
+          :model="handleForm"
+          label-width="90px"
+          :inline="true"
+          :rules="handleRules"
+          style="margin-top: 40px; margin-left: 46px; margin-right: 50px"
+        >
+          <el-form-item label="处置记录" prop="record" class="input2">
+            <el-input
+              placeholder="请输入"
+              v-model="handleForm.record"
+            ></el-input>
+          </el-form-item>
+          <el-form-item label="处置时间" class="input1">
+            <el-date-picker
+              v-model="handleForm.time"
+              type="datetime"
+              placeholder="请选择"
+              class="timeStyle"
+            ></el-date-picker>
+          </el-form-item>
+          <el-form-item label="处置人" class="input2">
+            <el-input
+              placeholder="请输入"
+              v-model="handleForm.people"
+            ></el-input>
+          </el-form-item>
+        </el-form>
+        <div style="height: 32px">
+          <div class="npdConfirm1" @click="handelConfirmClick">确定</div>
+          <div class="npdCancel1" @click="handelCancelClick">取消</div>
+        </div>
       </div>
     </el-dialog>
   </div>
@@ -220,19 +256,25 @@
 <script>
 export default {
   name: 'individual',
+  created () {
+    // 假数据
+    // for (let index = 0; index < 17; index++) {
+    //   this.policeList.push(this.policeListItem)
+    // }
+  },
   data () {
-    return {
-      searchImg: require('../../assets/images/policeHistory/search.png'),
-      belongOptions: [],
-      search: {
-        belong: '',
-        date: '',
-        other: ''
-      },
+    return (
+      {
+        searchImg: require('../../assets/images/policeHistory/search.png'),
+        belongOptions: [],
+        search: {
+          belong: '',
+          date: '',
+          other: ''
+        },
 
-      policeList: [
-        {
-          selected: true,
+        policeListItem: {
+          selected: false,
           xuHao: '1',
           bianHao: 'YZ63718FFB7380BL89',
           source: '群众举报',
@@ -244,84 +286,85 @@ export default {
           description: '疑似有非法捕捞船只',
           status: '未处置'
         },
-        {
-          selected: true,
-          xuHao: '2',
-          bianHao: 'YZ63718FFB7380BL89',
-          source: '群众举报',
-          people: '三儿',
-          phone: '88888888888',
-          address: '江夏区江夏区江夏区江夏区',
-          time: '2019-11-11 11:11:11',
-          belong: '武汉市渔政',
-          description: '疑似有非法捕捞船只',
-          status: '未处置'
+        policeList: [
+          {
+            selected: false,
+            xuHao: '1',
+            bianHao: 'YZ63718FFB7380BL89',
+            source: '群众举报',
+            people: '三儿',
+            phone: '88888888888',
+            address: '江夏区江夏区江夏区江夏区',
+            time: '8888-88-88 88:88:88',
+            belong: '武汉市渔政',
+            description: '疑似有非法捕捞船只',
+            status: '已处置'
+          },
+          {
+            selected: false,
+            xuHao: '1',
+            bianHao: 'YZ63718FFB7380BL89',
+            source: '群众举报',
+            people: '三儿',
+            phone: '88888888888',
+            address: '江夏区江夏区江夏区江夏区',
+            time: '8888-88-88 88:88:88',
+            belong: '武汉市渔政',
+            description: '疑似有非法捕捞船只',
+            status: '未处置'
+          },
+          {
+            selected: false,
+            xuHao: '1',
+            bianHao: 'YZ63718FFB7380BL89',
+            source: '群众举报',
+            people: '三儿',
+            phone: '88888888888',
+            address: '江夏区江夏区江夏区江夏区',
+            time: '8888-88-88 88:88:88',
+            belong: '武汉市渔政',
+            description: '疑似有非法捕捞船只',
+            status: '未处置'
+          }
+        ],
+        pageData: {
+          total: 100,
+          pageSize: 10,
+          currentPage: 1
         },
-        {
-          selected: true,
-          xuHao: '3',
-          bianHao: 'YZ63718FFB7380BL89',
-          source: '群众举报',
-          people: '三儿',
-          phone: '88888888888',
-          address: '江夏区江夏区江夏区江夏区',
-          time: '2019-11-11 11:11:11',
-          belong: '武汉市渔政',
-          description: '疑似有非法捕捞船只',
-          status: '未处置'
-        },
-        {
-          selected: true,
-          xuHao: '4',
-          bianHao: 'YZ63718FFB7380BL89',
-          source: '群众举报',
-          people: '三儿',
-          phone: '88888888888',
-          address: '江夏区江夏区江夏区江夏区',
-          time: '2019-11-11 11:11:11',
-          belong: '武汉市渔政',
-          description: '疑似有非法捕捞船只',
-          status: '未处置'
-        },
-        {
-          selected: true,
-          xuHao: '5',
-          bianHao: 'YZ63718FFB7380BL89',
-          source: '群众举报',
-          people: '三儿',
-          phone: '88888888888',
-          address: '江夏区江夏区江夏区江夏区',
-          time: '2019-11-11 11:11:11',
-          belong: '武汉市渔政',
-          description: '疑似有非法捕捞船只',
-          status: '未处置'
-        }
-      ],
-      multipleSelection: [],
-      pageData: {
-        total: 100,
-        pageSize: 10,
-        currentPage: 1
-      },
 
-      showDetail: false
-    }
+        showDetail: false,
+
+        showHandle: false,
+        handleForm: {
+          record: '',
+          time: '',
+          people: ''
+        },
+        handleRules: {},
+
+        currentIndex: -1,
+
+        selectedItem: [],
+
+        detailDlg: {
+          bianHao: '',
+          source: '',
+          people: '',
+          phone: '',
+          address: '',
+          time: '',
+          belong: '',
+          description: '',
+          mainRecord: '',
+          handleResult: '',
+          handleTime: '',
+          handlePeople: ''
+        }
+      }
+    )
   },
   methods: {
-    // 勾选变化
-    handleSelectionChange (val) {
-      this.multipleSelection = val
-      console.log(val)
-    },
-    // 处置
-    resolve (index, row) {
-      console.log(index, row)
-    },
-    // 显示案件详情
-    showDetailClick (index, row) {
-      console.log(index, row)
-      this.showDetail = true
-    },
     // 日期搜索
     dateSearch () {
       // if (this.date1) {
@@ -340,48 +383,43 @@ export default {
       // }
     },
     // 点击表格行
-    clickTableRow (row) {
-      // this.radio = this.firePoliceList.indexOf(row)
-      // var detail = this.firePoliceList[this.radio]
-      // if (detail.alarmPicList) {
-      //   this.fireDetailInfo.image1 = detail.alarmPicList[0].picPath
-      //   this.fireDetailInfo.image2 = detail.alarmPicList[1].picPath
-      // }
-      // this.fireDetailInfo.alarmTime = detail.alarmTime
-      // this.fireDetailInfo.alarmTypeName = detail.alarmTypeName
-      // this.fireDetailInfo.deviceName = detail.deviceName
-      // this.fireDetailInfo.alarmAddress = detail.alarmAddress
-      // this.fireDetailInfo.alarmStatus = detail.alarmStatus
-      // this.fireDetailInfo.updateTime = detail.updateTime
-      // this.fireDetailInfo.id = detail.id
-      // if (detail.alarmStatus === '确认' || detail.alarmStatus === '误报') {
-      //   this.fireDetailInfo.showConfirm = false
-      // } else {
-      //   this.fireDetailInfo.showConfirm = true
-      // }
-      // this.showFireDetail = true
-      // const p = this
-      // setTimeout(() => {
-      //   const tmpMap = p.$refs.gduMap.map2D
-      //   tmpMap._map.updateSize()
-      //   tmpMap.zoomToCenter(detail.alarmLongitude, detail.alarmLatitude)
-      //   tmpMap.customMarkerLayerManager.addMarker({
-      //     lon: detail.alarmLongitude,
-      //     lat: detail.alarmLatitude
-      //   })
-      //   tmpMap.setZoom(16)
-      // }, 500)
-    },
+    clickTableRow (row) {},
     // 分页页数改变
     currentPageChange () {
       // this.getFirePoliceList()
     },
+    // 勾选变化
+    handleSelectionChange (val) {
+      this.selectedItem = val
+      // console.log(val)
+    },
+    deleteClick () {
+      // console.log('shanchu')
+    },
+    // 显示案件详情
+    showDetailClick (index, row) {
+      // console.log(index, row)
+
+      this.showDetail = true
+    },
     // 关闭详情
     detailCancelClick () {
       this.showDetail = false
+    },
+    // 处置
+    resolve (index, row) {
+      // console.log(index, row)
+      this.showHandle = true
+    },
+    // 处置弹窗-确定
+    handelConfirmClick () {
+      this.showHandle = false
+    },
+    // 处置弹窗-取消
+    handelCancelClick () {
+      this.showHandle = false
     }
-  },
-  created () {}
+  }
 }
 </script>
 
@@ -542,6 +580,20 @@ export default {
     border-color: #c5f3ff;
   }
 }
+.unHandleBtn {
+  width: 74px;
+  height: 26px;
+  background: #1eb0fc;
+  font-size: 16px;
+  border: none;
+}
+.handleBtn {
+  width: 74px;
+  height: 26px;
+  background: rgba($color: #1eb0fc, $alpha: 0.5);
+  font-size: 16px;
+  border: none;
+}
 
 .detailDlg.el-dialog__wrapper {
   /deep/.el-dialog {
@@ -598,5 +650,88 @@ export default {
   color: white;
   font-size: 16px;
   margin-top: 25px;
+}
+
+.newPoliceDlg.el-dialog__wrapper {
+  /deep/.el-dialog {
+    .el-dialog__header {
+      display: none;
+    }
+    background: transparent;
+    .el-dialog__body {
+      display: inline-block;
+      padding: 0px;
+      width: 100%;
+      height: 679px;
+      background: url(../../assets/images/RealtimeDataMenu/newPoliceBox.png)
+        no-repeat;
+      background-size: 100%;
+      .npdTitleSty {
+        width: 166px;
+        height: 34px;
+        color: white;
+        font-size: 18px;
+        font-weight: bold;
+        line-height: 42px;
+        background: url(../../assets/images/header-bg.png) no-repeat;
+        background-size: 100%;
+        padding-left: 30px;
+        margin: 26px 0 0 24px;
+      }
+      .input1 {
+        .el-input__inner {
+          color: white;
+          width: 310px;
+          border: solid 1px #0fbfe0;
+          background-color: transparent;
+        }
+      }
+      .input2 {
+        .el-input__inner {
+          width: 760px;
+          color: white;
+          border: solid 1px #0fbfe0;
+          background-color: transparent;
+        }
+      }
+      .el-form-item__label {
+        color: #0fbfe0;
+        font-size: 16px;
+      }
+      .label1 {
+        margin-left: 40px;
+      }
+      .timeStyle {
+        width: 310px;
+      }
+    }
+  }
+}
+.npdConfirm1 {
+  float: right;
+  width: 87px;
+  height: 32px;
+  background: #1eb0fc;
+  color: white;
+  margin-right: 65px;
+  font-size: 18px;
+  line-height: 32px;
+  text-align: center;
+  border-radius: 4px;
+  cursor: pointer;
+}
+.npdCancel1 {
+  float: right;
+  width: 87px;
+  height: 32px;
+  background: transparent;
+  color: #1eb0fc;
+  margin-right: 32px;
+  font-size: 18px;
+  line-height: 32px;
+  text-align: center;
+  border-radius: 4px;
+  border: solid 1px #1eb0fc;
+  cursor: pointer;
 }
 </style>
