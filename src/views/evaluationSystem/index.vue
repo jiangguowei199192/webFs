abbd356e75f93bcda4d2d8d3b65315400d4baf88
yaochen <yaochen85@163.com>
2020-12-13 14:25:35 +0800

    完成部分可视化调度地图逻辑

--- src/views/evaluationSystem/index.vue
+++ src/views/evaluationSystem/index.vue
@@ -1,15 +1,13 @@
 <template>
-  <div class="evaluation" :style="'height:'+fullHeight+'px;'">
-    <gMap
-      ref="gduMap"
-      :bShowBasic="false"
-      :bShowMeasure="false"
-      :bShowLonLat="false"
-    ></gMap>
+  <div :style="'height:'+fullHeight+'px;'">
+    <gMap ref="gduMap" :bShowBasic="false" :bShowMeasure="false" :bShowLonLat="false"></gMap>
   </div>
 </template>
 
 <script>
+import videoMixin from '../videoSystem/mixins/videoMixin'
+import riverMixin from '../decisionSystem/riverMixin'
+import { EventBus } from '@/utils/eventBus.js'
 export default {
   name: 'evaluation',
   data () {
@@ -18,6 +16,7 @@ export default {
       fullHeight: 0
     }
   },
+  mixins: [videoMixin, riverMixin],
   methods: {
     setMapHeight () {
       const tmpMap = this.$refs.gduMap.map2D
@@ -27,6 +26,48 @@ export default {
       setTimeout(() => {
         tmpMap._map.updateSize()
       }, 200)
+    },
+    // 处理数据，便于地图组件加载显示
+    handerVideoDevice (dev) {
+      if (dev.deviceTypeCode === 'GDJK') {
+        dev.type = 'RP_Camera'
+      } else if (dev.deviceTypeCode === 'WRJ') {
+        dev.type = 'RP_Drone'
+      }
+      dev.name = dev.label
+      dev.address = dev.deviceAddress
+      dev.brand = dev.deviceBrand
+      dev.longitude = dev.deviceLongitude
+      dev.latitude = dev.deviceLatitude
+    },
+    // 新增显示高点或无人机设备
+    addDeviceCallback (devInfo) {
+      this.handerVideoDevice(devInfo)
+      this.$refs.gduMap.map2D._dispatchCenterManager.addRpDatas([devInfo])
+    },
+    // 显示高点设备和无人机设备
+    getAllDeviceDoneCallback (cameraDevs, droneDevs) {
+      cameraDevs.forEach(dev => {
+        this.handerVideoDevice(dev)
+      })
+      this.showRpDatas(cameraDevs)
+      droneDevs.forEach(dev => {
+        this.handerVideoDevice(dev)
+      })
+      this.showRpDatas(droneDevs)
+    },
+    // 显示长江大保护数据层信息
+    showRpDatas (tmpDatas) {
+      this.$refs.gduMap.map2D._dispatchCenterManager.addRpDatas(tmpDatas)
+      if (tmpDatas.length > 0) {
+        if (this.$refs.gduMap !== undefined) {
+          this.$refs.gduMap.map2D.zoomToCenter(
+            tmpDatas[0].longitude,
+            tmpDatas[0].latitude
+          )
+          this.$refs.gduMap.map2D.setZoom(12)
+        }
+      }
     }
   },
   mounted () {
@@ -35,12 +76,22 @@ export default {
       me.setMapHeight()
     }
     this.setMapHeight()
+    const tmpWarn = {
+      id: '6dddef0f25758f86db0b7281b0c2efa8',
+      caseNo: '2020092922678',
+      reportTel: '15672675664',
+      reportAddr: '汉口江滩',
+      reportTime: '2020-10-06 11:00:00',
+      caseDesc: null,
+      longitude: 114.316317,
+      latitude: 30.606778
+    }
+    setTimeout(() => {
+      EventBus.$emit('addNewWarningSuccess', tmpWarn)
+    }, 5000)
   }
 }
 </script>
 
 <style lang="scss" scoped>
-.evaluation {
-
-}
 </style>
